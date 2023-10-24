9983d9025d5520966c8d85b62653555aa5a09a50
MiskuZero <739740010@qq.com>
2023-10-25 03:07:07 +0800

    提交测试！

--- src/com/mikkku/launcher/Launcher.java
+++ src/com/mikkku/launcher/Launcher.java
@@ -10,7 +10,7 @@ public class Launcher {
     public static void main(String[] args) {
         // TODO: 2023/10/24 处理参数args，给多个路径不能是包含关系的路径
         new DuplicateFileScanner().scan(
-                new File("C:\\Users\\Administrator\\Desktop\\new world tmp")
+                new File("C:\\Users\\Administrator\\Desktop\\openJDK")
         );
     }
 
