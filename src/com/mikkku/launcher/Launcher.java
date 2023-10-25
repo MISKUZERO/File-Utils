004488fcbc7ad4b8939653b45bd1d4c08018cd2a
MiskuZero <739740010@qq.com>
2023-10-26 02:36:34 +0800

    可加入多个扫描线程，还有一个小问题（会漏文件）！

--- src/com/mikkku/launcher/Launcher.java
+++ src/com/mikkku/launcher/Launcher.java
@@ -7,7 +7,7 @@ import java.io.File;
 
 public class Launcher {
 
-    public static void main(String[] args) {//git测试
+    public static void main(String[] args) {
         // TODO: 2023/10/24 处理参数args，给多个路径不能是包含关系的路径
         new DuplicateFileScanner(
                 new File("C:\\Users\\Administrator\\Desktop\\new world tmp"),
